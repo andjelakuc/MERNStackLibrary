90410b4f11405776d6928fa4db5b3afbd6ed7f6a
andjelakuc <andjelakuc01@gmail.com>
2023-09-23 01:48:24 +0200

    registracija i prijava

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Form } from 'antd'
+import Button from '../../components/Button'
+import { Link } from 'react-router-dom';
 
 function Login() {
+
+    const onFinish = async (values) => {
+        console.log("Success:", values)
+      };
+
+
   return (
-    <div>Login</div>
+    <div className='h-screen bg-primary flex items-center justify-center'>
+        
+        <div className='authentication-form bg-third p-3 rounded'>
+        <h1 className="text-black text-2xl font-bold mb-1">
+          BIBLIOTEKA - PRIJAVA
+        </h1>
+        <hr />
+            <Form layout='vertical' onFinish={onFinish}>
+
+           <Form.Item
+           label="Email"
+           name="email">
+           <input type="email" placeholder="Email" />
+           </Form.Item>
+
+           <Form.Item
+           label="Šifra"
+           name="password">
+           <input type="password" placeholder="Šifra" />
+           </Form.Item>
+
+
+           <div className="text-center mt-2 flex flex-col gap-1">
+           <Button title="Prijava" type='submit' color='fourth'/>
+           <Link to="/register" className="text-black text-sm">
+              Još uvek nemate nalog? Registrujte se!
+            </Link>
+            </div>
+
+            </Form>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import { Form } from 'antd'
+import Button from '../../components/Button'
+import { Link } from 'react-router-dom';
 
 function Register() {
+
+    const onFinish = async (values) => {
+        console.log("Success:", values)
+      };
+
+
   return (
-    <div className='h-screen bg-primary'>Register</div>
+    <div className='h-screen bg-primary flex items-center justify-center'>
+        
+        <div className='authentication-form bg-third p-3 rounded'>
+        <h1 className="text-black text-2xl font-bold mb-1">
+          BIBLIOTEKA - REGISTRACIJA
+        </h1>
+        <hr />
+            <Form layout='vertical' onFinish={onFinish}>
+
+           <Form.Item
+           label="Ime"
+           name="name">
+           <input type="text" placeholder="Ime" />
+           </Form.Item>
+
+           <Form.Item
+           label="Email"
+           name="email">
+           <input type="email" placeholder="Email" />
+           </Form.Item>
+
+           <Form.Item
+           label="Šifra"
+           name="password">
+           <input type="password" placeholder="Šifra" />
+           </Form.Item>
+
+
+           <div className="text-center mt-2 flex flex-col gap-1">
+           <Button title="Registracija" type='submit' color='fourth'/>
+           <Link to="/login" className="text-black text-sm">
+              Već imate nalog? Prijavite se!
+            </Link>
+            </div>
+
+            </Form>
+        </div>
+    </div>
   )
 }
 
